e5cc8eb5937654a8912fc9b84a7e7339e4be6a75
scue <linkscue@gmail.com>
2014-05-16 21:44:47 +0800

    stuttgart.mk: merge the OLD repo stuttgart.mk

--- stuttgart.mk
+++ stuttgart.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+DEVICE_PACKAGE_OVERLAYS += device/lenovo/stuttgart/overlay
 LOCAL_BASEDIR=device/lenovo/stuttgart
 
 # overlay
@@ -28,13 +29,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
+# Gps
+PRODUCT_COPY_FILES += \
+    device/lenovo/stuttgart/configs/gpsconfig.xml:system/etc/gpsconfig.xml
+
 # This device is xhdpi.  However the platform doesn't
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
 # Boot: root dir
 PRODUCT_COPY_FILES := \
     $(LOCAL_BASEDIR)/rootdir/gpio:root/sbin/gpio \
@@ -106,8 +110,210 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
     ro.allow.mock.location=1 \
     ro.adb.secure=0 \
-    ro.debuggable=1 
+    ro.debuggable=1
 
+# System: firmware
+PRODUCT_COPY_FILES += \
+	device/lenovo/stuttgart/firmware/fw_bcmdhd_apsta.bin:system/etc/firmware/fw_bcmdhd_apsta.bin \
+	device/lenovo/stuttgart/firmware/fw_bcmdhd.bin:system/etc/firmware/fw_bcmdhd.bin \
+	device/lenovo/stuttgart/firmware/fw_bcmdhd_p2p.bin:system/etc/firmware/fw_bcmdhd_p2p.bin \
+	device/lenovo/stuttgart/firmware/sdio-g-mfgtest.bin:system/etc/firmware/sdio-g-mfgtest.bin
+
+# System: rril
+PRODUCT_COPY_FILES += \
+	device/lenovo/stuttgart/rril/repository.txt:system/etc/rril/repository.txt \
+	device/lenovo/stuttgart/rril/stmd.conf:system/etc/rril/stmd.conf
+
+# System: Audio
+PRODUCT_COPY_FILES += \
+	device/lenovo/stuttgart/configs/asound.conf:system/etc/asound.conf \
+	device/lenovo/stuttgart/configs/audio_policy.conf:system/etc/audio_policy.conf \
+	device/lenovo/stuttgart/alsa/alsa.conf:system/usr/share/alsa/alsa.conf \
+	device/lenovo/stuttgart/alsa/cards/aliases.conf:system/usr/share/alsa/cards/aliases.conf \
+	device/lenovo/stuttgart/alsa/pcm/center_lfe.conf:system/usr/share/alsa/pcm/center_lfe.conf \
+	device/lenovo/stuttgart/alsa/pcm/default.conf:system/usr/share/alsa/pcm/default.conf \
+	device/lenovo/stuttgart/alsa/pcm/dmix.conf:system/usr/share/alsa/pcm/dmix.conf \
+	device/lenovo/stuttgart/alsa/pcm/dpl.conf:system/usr/share/alsa/pcm/dpl.conf \
+	device/lenovo/stuttgart/alsa/pcm/dsnoop.conf:system/usr/share/alsa/pcm/dsnoop.conf \
+	device/lenovo/stuttgart/alsa/pcm/front.conf:system/usr/share/alsa/pcm/front.conf \
+	device/lenovo/stuttgart/alsa/pcm/iec958.conf:system/usr/share/alsa/pcm/iec958.conf \
+	device/lenovo/stuttgart/alsa/pcm/modem.conf:system/usr/share/alsa/pcm/modem.conf \
+	device/lenovo/stuttgart/alsa/pcm/rear.conf:system/usr/share/alsa/pcm/rear.conf \
+	device/lenovo/stuttgart/alsa/pcm/side.conf:system/usr/share/alsa/pcm/side.conf \
+	device/lenovo/stuttgart/alsa/pcm/surround40.conf:system/usr/share/alsa/pcm/surround40.conf \
+	device/lenovo/stuttgart/alsa/pcm/surround41.conf:system/usr/share/alsa/pcm/surround41.conf \
+	device/lenovo/stuttgart/alsa/pcm/surround50.conf:system/usr/share/alsa/pcm/surround50.conf \
+	device/lenovo/stuttgart/alsa/pcm/surround51.conf:system/usr/share/alsa/pcm/surround51.conf \
+	device/lenovo/stuttgart/alsa/pcm/surround71.conf:system/usr/share/alsa/pcm/surround71.conf
+
+# System: vold
+PRODUCT_COPY_FILES += \
+    device/lenovo/stuttgart/configs/vold.fstab:system/etc/vold.fstab
+
+# System: Wifi
+PRODUCT_COPY_FILES += \
+    device/lenovo/stuttgart/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=15
+
+#
+# PACKAGE
+#
+# TODO: preload
+PRODUCT_PACKAGES += \
+	hcitool
+
+# TODO: DeviceSettings
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory \
+	Torch
+
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.primary.smdk4x12 \
+	audio.usb.default \
+	Camera
+
+PRODUCT_PACKAGES += \
+    sdcard
+
+PRODUCT_PACKAGES += \
+    libsecmfcdecapi \
+    libsecmfcencapi
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+    Galaxy4 \
+    HoloSpiralWallpaper \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    MagicSmokeWallpapers \
+    NoiseField \
+    PhaseBeam \
+    VisualizationWallpapers \
+    librs_jni
+
+# FS management tools
+PRODUCT_PACKAGES += \
+    static_busybox \
+    make_ext4fs \
+    e2fsck \
+    setup_fs
+
+# WLAN
+PRODUCT_PACKAGES += \
+	libnetcmdiface
+
+# OMX
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libSEC_OMX_Resourcemanager \
+    libSEC_OMX_Core \
+    libOMX.SEC.AVC.Decoder \
+    libOMX.SEC.M4V.Decoder \
+    libOMX.SEC.WMV.Decoder \
+    libOMX.SEC.AVC.Encoder \
+    libOMX.SEC.M4V.Encoder \
+    libOMX.SEC.M2V.Decoder
+#   libOMX.SEC.VP8.Decoder
+
+PRODUCT_COPY_FILES += \
+    device/lenovo/stuttgart/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/lenovo/stuttgart/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    device/lenovo/stuttgart/bluetooth/libbt-vendor.so:system/lib/libbt-vendor.so
+
+#
+# TODO: SuperCAM
+#
+
+#
+# TODO: mkshrc
+#
+
+# RIL
+BOARD_PROVIDES_LIBRIL := true
+
+# System: build.prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.strictmode.disable=1 \
+    mobiledata.interfaces=pdp0,wlan0,gprs,ppp0,rmnet0 \
+    ro.ril.hsxpa=1 \
+    ro.ril.gprsclass=10 \
+    persist.sys.timezone=Asia/Shanghai \
+    persist.sys.language=zh \
+    persist.sys.country=CN \
+    ro.config.ringtone=Salt_water.ogg \
+    ro.config.notification_sound=Heaven_nearby.ogg \
+    ro.config.alarm_alert=Dreamland.ogg
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072 \
+    hwui.render_dirty_regions=false
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mass_storage \
+    ro.build.display.id=lephone.cc_linkscue_cm10.1
+
+#
+# HAL
+#
+# MARK: 2014年05月16日
+# rm lights.exynos4 libhdmi libhdmiclient libasan_preload
+PRODUCT_PACKAGES += \
+	camera.stuttgart \
+	hwcomposer.exynos4
+#	libsecril-client
+#   libgralloc_ump \
+#   libhwconverter \
+#   libfimg \
+#	libTVOut \
+#	libhwjpeg \
+#	libfimc
+#  	libcec \
+#   libddc \
+#   libedid \
+#   libtinyalsa \
+
+# COMMON: These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    device/lenovo/stuttgart/prebuilt/com.yamaha.android.media.xml:system/etc/permissions/com.yamaha.android.media.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+
+# TODO: pls fix wifi,bluetooth..
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
+# Include exynos4 platform specific parts
+TARGET_HAL_PATH := hardware/samsung/exynos4/hal
+TARGET_OMX_PATH := hardware/samsung/exynos/multimedia/openmax
+#$(call inherit-product, hardware/samsung/exynos4x12.mk)
+#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
 $(call inherit-product-if-exists, vendor/lenovo/stuttgart/stuttgart-vendor.mk)
+#$(call inherit-product-if-exists, vendor/le/packages.mk)
